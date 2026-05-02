5ee1939ddfd8b1ba9e01f3af6e72723c954ba251
algareeb01-ui <algareeb01@gmail.com>
2026-05-02 16:58:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,12 @@ app = FastAPI()
 @app.post("/talk")
 async def talk():
     try:
-        # النص اللي زيزو حيقوله
-        text = "يا خالد، أنا زيزو، دلوقت السيرفر شغال بأقل استهلاك ومستعد للأوامر."
-        
-        # تحويل النص لصوت
+        text = "يا خالد، أنا زيزو، بنجرب نهدي الكلاشنكوف ونسمع الصوت."
         communicate = edge_tts.Communicate(text, "ar-EG-ShakirNeural")
         audio_data = b""
         async for chunk in communicate.stream():
             if chunk["type"] == "audio":
                 audio_data += chunk["data"]
-        
-        # إرسال البيانات كـ Stream
         return Response(content=audio_data, media_type="application/octet-stream")
     except Exception as e:
         return Response(content=str(e), status_code=500)
